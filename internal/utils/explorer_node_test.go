087e790cc1b76aa7124595c3e8ee76f0f23e8909
Umair Abid <umairac90@gmail.com>
2026-03-12 17:39:30 -0700

    Add specs for explorer list

--- internal/utils/explorer_node_test.go
+++ internal/utils/explorer_node_test.go
@@ -17,3 +17,45 @@ func TestExplorerListInitialize(t *testing.T) {
 		}
 	})
 }
+
+func TestExplorerListChildren(t *testing.T) {
+	list := ExplorerList{}
+	list.Initialize()
+
+	children := []ExplorerNode{
+		{Title: "Child 1", Type: "child"},
+		{Title: "Child 2", Type: "child"},
+		{Title: "Child 3", Type: "child"},
+	}
+
+	t.Run("Should set children correctly", func(t *testing.T) {
+		list.Expand(children)
+		if len(list.Selected.Children) != 2 {
+			t.Fatalf("Expected 2 children, got %d", len(list.Selected.Children))
+		}
+	})
+
+	t.Run("Should set selected as first node previous", func(t *testing.T) {
+		if list.Selected.Children[0].Previous != list.Selected {
+			t.Errorf("Expected first child Previous to be selected node, got %v", list.Selected.Children[0].Previous)
+		}
+	})
+
+	t.Run("Should set second child as next of first child", func(t *testing.T) {
+		if list.Selected.Children[0].Next != &list.Selected.Children[1] {
+			t.Errorf("Expected first child Next to be second child, got %v", list.Selected.Children[0].Next)
+		}
+	})
+
+	t.Run("Should set next of last child to next of selected node", func(t *testing.T) {
+		if list.Selected.Children[2].Next != list.Selected.Next {
+			t.Errorf("Expected last child Next to be selected node Next, got %v", list.Selected.Children[1].Next)
+		}
+	})
+
+	t.Run("Should set previous of first child to previous of selected node", func(t *testing.T) {
+		if list.Selected.Children[0].Previous != list.Selected.Previous {
+			t.Errorf("Expected first child Previous to be selected node Previous, got %v", list.Selected.Children[0].Previous)
+		}
+	})
+}
